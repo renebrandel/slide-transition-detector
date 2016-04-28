136eabca87997b211d7acecc9e348ca0b8b0d1bf
Rene Brandel <renebrandel@outlook.com>
2016-04-28 09:46:02 +0200

    added cleanup script, so that the folders are not bloated

---
+++ cleanup.py
@@ -0,0 +1,11 @@
+import os
+folders = ['img', 'matches', 'slides']
+
+for folder in folders:
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print(e)

c0e84d964e5f1fe738eb364486966eb4febd842a
Rene Brandel <renebrandel@outlook.com>
2016-04-28 10:48:40 +0200

    Added ImageWriter class to clean up the code and introduce more visibility

--- detector.py
+++ detector.py
@@ -5,6 +5,18 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+class ImageWriter:
+
+    def __init__(self, prefix='img ', file_format='.jpg', count=0):
+        self.count = count
+        if not file_format.startswith('.'):
+            file_format = '.' + file_format
+        self.name = prefix + '%d' + file_format
+
+    def write_image(self, img):
+        cv2.imwrite(self.name % self.count, img)
+
+
 def sanitize_device(device):
     """returns device id if device can be converted to an integer"""
     try:
@@ -15,19 +27,64 @@ def sanitize_device(device):
 
 def main(args):
     """Main execution function"""
+    setup_dirs()
 
+    # get the video stream
     cap = cv2.VideoCapture(sanitize_device(args.device))
 
+    # setup output video (if needed)
+    """
     size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
             int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
     fps = cap.get(cv2.CAP_PROP_FPS)
-
     codec = cv2.VideoWriter_fourcc('X', '2', '6', '4')
     out = cv2.VideoWriter(args.outfile, codec, fps, size)
+    """
+
+    _, last_frame = cap.read()
+
+    slide_writer = ImageWriter('slides/slide ')
+    slide_writer.write_image(last_frame)
+
+    interval_writer = ImageWriter('img/out ')
 
     i = 0
-    print ('starting')
 
+    while True:
+
+        ret, frame = cap.read()
+        if ret:
+            if i % 100 == 0:
+                # every 100th frame it writes the image to disk
+                interval_writer.write_image(frame)
+                # out.write(frame) # write video
+
+            if not are_same(last_frame, frame, i):
+                slide_writer.write_image(frame)
+
+            i += 1
+            last_frame = frame
+        else:
+            break
+    print ('done')
+
+    print i
+    cap.release()
+    # out.release()
+    cv2.destroyAllWindows()
+
+
+def are_same(fst, snd, count):
+    res = cv2.subtract(snd, fst)
+    hist = cv2.calcHist([res], [0], None, [256], [0, 256])
+    similarity = 1 - np.sum(hist[15::]) / np.sum(hist)
+    print count, similarity
+    if similarity > 0.99:
+        return True
+    return False
+
+
+def setup_dirs():
     if not os.path.exists('img'):
         os.makedirs('img')
 
@@ -36,45 +93,6 @@ def main(args):
 
     if not os.path.exists('matches'):
         os.makedirs('matches')
-
-    _, last_frame = cap.read()
-    cv2.imwrite('slides/slide %d.jpg' % 0, last_frame)
-    count = 0
-
-    while True:
-
-        ret, frame = cap.read()
-        if ret:
-            if i % 100 == 0:
-                # every 100th frame it writes the image to disk
-                cv2.imwrite('img/out_%d.jpg' % i, frame)
-                out.write(frame)
-
-            i += 1
-            if not are_same(last_frame, frame, i):
-                count += 1
-                cv2.imwrite('slides/slide %d.jpg' % count, frame)
-                # print np.subtract(last_frame, frame)
-            last_frame = frame
-        else:
-            break
-    print ('done')
-
-    print i
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()
-
-
-def are_same(fst, snd, count):
-    res = cv2.subtract(snd, fst)
-    cv2.imwrite('matches/match %d.jpg' % count, res)
-    hist = cv2.calcHist([res], [0], None, [256], [0, 256])
-    similarity = 1 - np.sum(hist[15::]) / np.sum(hist)
-    print count, similarity
-    if similarity > 0.99:
-        return True
-    return False
 
 
 if __name__ == "__main__":

fb4741b813a47b6963ddcf982c5a1ca394fffe6c
Rene Brandel <renebrandel@outlook.com>
2016-05-07 13:02:14 +0200

    Completed MVP of sorter process. Now it actual outputs the files into a folder and gives the user a time schedule of when a slide should appear when

--- sorter.py
+++ sorter.py
@@ -1,9 +1,11 @@
 import cv2
 import os
+import mediaoutput
 import imgcomparison
 
 
 def sort(dir):
+    mediaoutput.setup_dirs('unique/')
     slides = []
     for filename in sorted(os.listdir(dir)):
         file_path = os.path.join(dir, filename)
@@ -22,21 +24,37 @@ def sort(dir):
 
         for j in xrange(i, len(slides)):
             other = slides[j]
-            if slide == other:
+            if slide == other or other.marked:
                 continue
 
             if comparator.are_same(slide.img, other.img):
-                if other.marked:
-                    slide.marked = True
+                slide.add_time(other.time)
                 other.marked = True
-                print slide.name, other.name
+
+    unique_slides = filter(lambda x: not x.marked, slides)
+    out = mediaoutput.IncrementalImageWriter(prefix='unique/', start=1)
+    timetable = open('unique/timetable.txt','w')
+    i = 1
+    for slide in unique_slides:
+        out.write(slide.img)
+        x = slide.time
+        for com in slide.times:
+            x += " " + com
+        timetable.write("Slide %d: %s\n" % (i, x))
+        i += 1
+    timetable.close()
 
 
 class Slide(object):
-    def __init__(self, name, img):
-        self.name = name
+
+    def __init__(self, time, img):
+        self.time, _ = os.path.splitext(time)
         self.img = img
         self.marked = False
+        self.times = []
+
+    def add_time(self, time):
+        self.times.append(time)
 
 
 def file_supported(ext):

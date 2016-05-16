5e78741a3cd2492e2fe952786344b44e7201bd46
Rene Brandel <renebrandel@outlook.com>
2016-05-16 10:00:41 +0200

    Created Minimum Viable Product of content extractor

--- extractor.py
+++ extractor.py
@@ -1,8 +1,21 @@
-from PIL import Image
 import pyocr
 import pyocr.builders
-
+from slides import SlideDataHelper
 
 
 if __name__ == "__main__":
-    pass
+
+    recognizer = pyocr.get_available_tools()[0]
+    contents = []
+
+    slides = SlideDataHelper("unique/", image_type="pil").get_slides()
+    for slide in slides:
+        txt = recognizer.image_to_string(slide.img, lang="deu", builder=pyocr.builders.TextBuilder())
+        contents.append(txt)
+    count = 0
+    for line in contents:
+        print "Slide %d" % count
+        print line
+        print "--------------------------------------"
+        print
+        count += 1

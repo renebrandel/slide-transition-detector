2f8de4368f4d614472386a865314c4f4b82417f4
Rene Brandel <renebrandel@outlook.com>
2016-06-09 10:25:24 +0200

    Restructured main.py to be a batch processor for all three pipeline tasks

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import argparse
-import imgcomparison as ic
-
+import os.path as p
 import sources
 from detector import Detector
 from sorter import SlideSorter
 from extractor import ContentExtractor
 
+
+def execute(inputfile, extractor_out, detector_out=None, sorter_out=None):
+    detector = sources.ListSource(Detector(inputfile, outpath=detector_out).detect_slides())
+    sorter = sources.ListSource(SlideSorter(detector, outpath=sorter_out).sort())
+    extractor = ContentExtractor(sorter, output_dir=extractor_out)
+    extractor.analyze()
+
+
+def batchExecute(inputfiles, extractor_out="contents/", detector_out="detected_slides/", sorter_out="sorted_slides"):
+    for file in inputfiles:
+        name = p.basename(file)
+        extractor = p.join(extractor_out, name) + p.sep
+        sorter = p.join(sorter_out, name) + p.sep
+        detector = p.join(detector_out, name) + p.sep
+        print "Analyzing " + name
+        print
+        execute(file, extractor, detector, sorter)
+        print
+
 if __name__ == "__main__":
 
     Parser = argparse.ArgumentParser(description="Slide Detector")
-    Parser.add_argument("-d", "--device", help="video device number or path to video file")
-    Parser.add_argument("-o", "--outpath", help="path to output video file", default="slides/", nargs='?')
-    Parser.add_argument("-f", "--fileformat", help="file format of the output images e.g. '.jpg'",
-                        default=".jpg", nargs='?')
-    Parser.add_argument("-t", "--timetable",
-                        help="path where the timetable should be written (default is the outpath+'timetable.txt')",
-                        nargs='?', default=None)
+    Parser.add_argument("files", help="video device number or path to video file", nargs='+')
     Args = Parser.parse_args()
-
-    detector = sources.ListSource(Detector(Args.device).detect_slides())
-    sorter = sources.ListSource(SlideSorter(detector).sort())
-    extractor = ContentExtractor(sorter, "contents/")
-    extractor.analyze()
+    batchExecute(Args.files)

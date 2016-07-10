ec2fa53585ab17e3275d45d00ca55433f9bf6065
Rene Brandel <renebrandel@outlook.com>
2016-07-10 15:41:48 +0200

    Evaluation script can now create videos from pdf files

--- eval.py
+++ eval.py
@@ -1,4 +1,28 @@
 import subprocess
+import argparse
+import os
 
-for i in xrange(20):
-    subprocess.call(['touch', 'hello_world'])
+png_prefix = 'slide'
+
+
+def convertToPNG(pdf):
+    foldername, _ = os.path.splitext(pdf)
+    foldername = os.path.basename(foldername)
+    foldername = os.path.join('PNG_Seq', foldername)
+    subprocess.call(['mkdir', '-p' ,foldername])
+    subprocess.call(['convert', pdf, foldername, os.path.join(foldername, png_prefix + '.png')], shell=False)
+    return foldername
+
+def stitch(sequence):
+    subprocess.call(['./stitcher.sh', '-p', os.path.join(sequence, 'slide-'), '-f', 'png', '-i', '0', '-o', sequence])
+
+if __name__ == "__main__":
+    Parser = argparse.ArgumentParser(description="Evaluation")
+    Parser.add_argument("slides", help="PDF files of the slides", nargs='+')
+    Args = Parser.parse_args()
+    png_sequences = []
+    for slide in Args.slides:
+        png_sequences.append(convertToPNG(slide))
+    for sequence in png_sequences:
+        stitch(sequence)
+
